05dd1dc032a0f8e9f68e10db36122aff694649f0
Logan Dhillon <logandhi64@gmail.com>
2023-12-09 16:35:14 -0500

    Initial Commit

---
+++ EduSpark/EduSparkApp.swift
@@ -0,0 +1,17 @@
+//
+//  EduSparkApp.swift
+//  EduSpark
+//
+//  Created by Logan Dhillon on 2023-12-09.
+//
+
+import SwiftUI
+
+@main
+struct EduSparkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
