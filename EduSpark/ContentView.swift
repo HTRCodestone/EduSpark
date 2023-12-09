75cb8b4a72bbed84f71c104f6089977cf0847b6c
Logan Dhillon <logandhi64@gmail.com>
2023-12-09 17:32:54 -0500

    Create SwiftUI project

--- EduSpark/ContentView.swift
+++ EduSpark/ContentView.swift
@@ -9,13 +9,78 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack() {
+            Text("All of your child's learning in one place")
+                .font(Font.custom("Helvetica Neue", size: 128).weight(.bold))
+                .foregroundColor(textColor)
+                .offset(x: 0, y: -95.58)
+                .multilineTextAlignment(.center)
+            ZStack() {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 540, height: 128)
+                    .background(Color(red: 0.01, green: 0.52, blue: 0.78))
+                    .cornerRadius(16)
+                    .offset(x: 0, y: 0)
+                Text("Sign up")
+                    .font(Font.custom("Helvetica Neue", size: 48).weight(.bold))
+                    .foregroundColor(Color(red: 0.89, green: 0.91, blue: 0.94))
+                    .offset(x: 0, y: 0)
+            }
+            .frame(width: 540, height: 128)
+            .offset(x: -286, y: 189)
+            ZStack() {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 540, height: 128)
+                    .background(Color(red: 0.65, green: 0.95, blue: 0.99))
+                    .cornerRadius(16)
+                    .offset(x: 0, y: 0)
+                Text("Learn more")
+                    .font(Font.custom("Helvetica Neue", size: 48).weight(.bold))
+                    .foregroundColor(Color(red: 0.06, green: 0.09, blue: 0.16))
+                    .offset(x: 0, y: 0)
+            }
+            .frame(width: 540, height: 128)
+            .offset(x: 286, y: 189)
+            ZStack() {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 128, height: 64)
+                    .background(Color(red: 0.09, green: 0.15, blue: 0.33))
+                    .cornerRadius(16)
+                    .offset(x: 0, y: 0)
+                Text("Log in")
+                    .font(Font.custom("Helvetica Neue", size: 24).weight(.bold))
+                    .foregroundColor(Color(red: 0.88, green: 0.95, blue: 1))
+                    .offset(x: 0, y: 0)
+            }
+            .frame(width: 128, height: 64)
+            .offset(x: 704, y: -476)
+            ZStack() {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 128, height: 64)
+                    .background(Color(red: 0.01, green: 0.52, blue: 0.78))
+                    .cornerRadius(16)
+                    .offset(x: 0, y: 0)
+                Text("Sign up")
+                    .font(Font.custom("Helvetica Neue", size: 24).weight(.bold))
+                    .foregroundColor(Color(red: 0.88, green: 0.95, blue: 1))
+                    .offset(x: 0, y: 0)
+            }
+            .frame(width: 128, height: 64)
+            .offset(x: 864, y: -476)
+            Rectangle()
+                .foregroundColor(.clear)
+                .frame(width: 302.91, height: 44.80)
+                .background(
+                    AsyncImage(url: URL(string: "https://via.placeholder.com/303x45"))
+                )
+                .offset(x: -776.54, y: -475.60)
         }
-        .padding()
+        .frame(width: 1920, height: 1080)
+        .background(Color(red: 0.96, green: 0.96, blue: 0.96))
     }
 }
 
---
+++ EduSpark/Globals.swift
@@ -0,0 +1,10 @@
+//
+//  Globals.swift
+//  EduSpark
+//
+//  Created by Logan Dhillon on 2023-12-09.
+//
+
+import SwiftUI
+
+var textColor = Color(red: 0.06, green: 0.09, blue: 0.16)
