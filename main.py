916c379c21fa206c9148e93b774a11439f660949
blaze <Arnnav0kudale@gmail.com>
2023-12-09 14:15:16 -0500

    very cool commit

--- main.py
+++ main.py
@@ -1,40 +1,51 @@
-import fitz  # PyMuPDF
+import urllib.request
 
-def extract_text_from_page(page):
-    # Extract text from the page
-    return page.get_text()
+def generate_curriculum_url(subject, grade):
+    # Special cases for specific subjects
+    if subject.lower() == "computer studies":
+        return "https://www.edu.gov.on.ca/eng/curriculum/secondary/computer10to12_2008.pdf"
 
-def pdf_to_text_with_structure(pdf_path, text_file_path):
-    # Open the PDF file
-    pdf_document = fitz.open(pdf_path)
+    if subject.lower() == "english":
+        # Use <grade>currb.pdf for English
+        return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/english{grade}currb.pdf"
 
-    # Create a text file for writing
-    with open(text_file_path, 'w', encoding='utf-8') as text_file:
-        # Iterate through each page in the PDF
-        for page_number in range(pdf_document.page_count):
-            # Get the page
-            page = pdf_document[page_number]
+    if subject.lower() == "mathematics":
+        # Use math<grade>curr.pdf for Mathematics
+        return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/math{grade}curr.pdf"
 
-            # Extract text from the page
-            page_text = extract_text_from_page(page)
+    if subject.lower() == "science":
+        # Use specific links for Science based on the grade
+        if grade in [9, 10]:
+            return "https://www.edu.gov.on.ca/eng/curriculum/secondary/science910_2008.pdf"
+        elif grade in [11, 12]:
+            return "https://www.edu.gov.on.ca/eng/curriculum/secondary/2009science11_12.pdf"
 
-            # Identify headers and subheaders based on font size
-            lines = page_text.split('\n')
-            for line in lines:
-                font_size = line.split(" ")[0]
-                if font_size.isdigit() and int(font_size) > 10:
-                    text_file.write(f"\n\n=== Header ===\n{line}\n")
-                elif font_size.isdigit() and int(font_size) > 8:
-                    text_file.write(f"\n--- Subheader ---\n{line}\n")
-                else:
-                    text_file.write(line + '\n')
+    # Default case for other subjects
+    grade_str = '910' if grade in [9, 10] else str(grade)
+    return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/{subject.lower()}{grade_str}curr.pdf"
 
-    # Close the PDF document
-    pdf_document.close()
+def download_file(url, destination):
+    urllib.request.urlretrieve(url, destination)
 
+def main():
+    # Get subject and grade from the user
+    subject = input("Enter the subject: ")
+    grade = int(input("Enter the grade (9, 10, 11, or 12): "))
 
-# Example usage:
-pdf_path = r'C:\Users\arnna\Downloads\english910currb.pdf'
-text_file_path = r'C:\Users\arnna\Downloads\ez.txt'
+    # Check if the grade is valid (9, 10, 11, or 12)
+    if grade not in [9, 10, 11, 12]:
+        print("Invalid grade. Please enter a valid grade (9, 10, 11, or 12).")
+        return
 
-pdf_to_text_with_structure(pdf_path, text_file_path)
+    # Generate the URL
+    url = generate_curriculum_url(subject, grade)
+
+    # Download the file
+    destination = f"{subject.lower()}_grade{grade}_curriculum.pdf"
+    download_file(url, destination)
+
+    # Print the success message
+    print(f"File downloaded successfully to {destination}")
+
+if __name__ == "__main__":
+    main()
