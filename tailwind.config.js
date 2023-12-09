86352f0779bebd0b2c1530df582c1677cd052fde
Logan Dhillon <logandhi64@gmail.com>
2023-12-09 15:24:29 -0500

    Theme stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    fontSize: {
+      huge: '8rem'
+    },
+    colors: {
+      primary: "#0284C7",
+      'dark-primary': "#172554",
+      'light-primary': "#A5F3FC",
+      accent: "#EAB308",
+      background: "#F5F5F5",
+      text: "#0F172A",
+      'secondary-text': "#4B5563"
+    }
   },
   plugins: [],
 }
