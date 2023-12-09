2cb64a006ec50df70c6a9044a785f3b388fb0a2f
blaze <Arnnav0kudale@gmail.com>
2023-12-09 14:22:00 -0500

    made it into a funcitno to be usabel in other files

--- main.py
+++ main.py
@@ -1,41 +1,33 @@
+import os
 import urllib.request
 
-def generate_curriculum_url(subject, grade):
-    # Special cases for specific subjects
-    if subject.lower() == "computer studies":
-        return "https://www.edu.gov.on.ca/eng/curriculum/secondary/computer10to12_2008.pdf"
+def download_curriculum(subject, grade):
+    def generate_curriculum_url(sub, grd):
+        if sub.lower() == "computer studies":
+            return "https://www.edu.gov.on.ca/eng/curriculum/secondary/computer10to12_2008.pdf"
 
-    if subject.lower() == "english":
-        # Use <grade>currb.pdf for English
-        return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/english{grade}currb.pdf"
+        if sub.lower() == "english":
+            return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/english{grd}currb.pdf"
 
-    if subject.lower() == "mathematics":
-        # Use math<grade>curr.pdf for Mathematics
-        return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/math{grade}curr.pdf"
+        if sub.lower() == "mathematics":
+            return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/math{grd}curr.pdf"
 
-    if subject.lower() == "science":
-        # Use specific links for Science based on the grade
-        if grade in [9, 10]:
-            return "https://www.edu.gov.on.ca/eng/curriculum/secondary/science910_2008.pdf"
-        elif grade in [11, 12]:
-            return "https://www.edu.gov.on.ca/eng/curriculum/secondary/2009science11_12.pdf"
+        if sub.lower() == "science":
+            if grd in [9, 10]:
+                return "https://www.edu.gov.on.ca/eng/curriculum/secondary/science910_2008.pdf"
+            elif grd in [11, 12]:
+                return "https://www.edu.gov.on.ca/eng/curriculum/secondary/2009science11_12.pdf"
 
-    # Default case for other subjects
-    grade_str = '910' if grade in [9, 10] else str(grade)
-    return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/{subject.lower()}{grade_str}curr.pdf"
+        grade_str = '910' if grd in [9, 10] else str(grd)
+        return f"https://www.edu.gov.on.ca/eng/curriculum/secondary/{sub.lower()}{grade_str}curr.pdf"
 
-def download_file(url, destination):
-    urllib.request.urlretrieve(url, destination)
-
-def main():
-    # Get subject and grade from the user
-    subject = input("Enter the subject: ")
-    grade = int(input("Enter the grade (9, 10, 11, or 12): "))
+    def download_file(url, destination):
+        urllib.request.urlretrieve(url, destination)
 
     # Check if the grade is valid (9, 10, 11, or 12)
     if grade not in [9, 10, 11, 12]:
         print("Invalid grade. Please enter a valid grade (9, 10, 11, or 12).")
-        return
+        return None
 
     # Generate the URL
     url = generate_curriculum_url(subject, grade)
@@ -44,8 +36,7 @@ def main():
     destination = f"{subject.lower()}_grade{grade}_curriculum.pdf"
     download_file(url, destination)
 
-    # Print the success message
-    print(f"File downloaded successfully to {destination}")
+    # Return the path
+    return os.path.abspath(destination)
 
-if __name__ == "__main__":
-    main()
+downloaded_file_path = download_curriculum(subject_input, grade_input) # returns path to the file
