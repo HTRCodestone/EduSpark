08b18c46078418017e1ca8f7023c57a673550d06
Logan Dhillon <logandhi64@gmail.com>
2023-12-09 18:37:53 -0500

    Create main page and ai page

---
+++ EduSpark/MainPage.swift
@@ -0,0 +1,18 @@
+//
+//  MainPage.swift
+//  EduSpark
+//
+//  Created by Logan Dhillon on 2023-12-09.
+//
+
+import SwiftUI
+
+struct MainPage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainPage()
+}
---
+++ EduSpark/Recommendations.swift
@@ -0,0 +1,18 @@
+//
+//  Recommendations.swift
+//  EduSpark
+//
+//  Created by Logan Dhillon on 2023-12-09.
+//
+
+import SwiftUI
+
+struct Recommendations: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Recommendations()
+}
