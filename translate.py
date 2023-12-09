4fe709f86fdafc5360f933ff82f7460cc93f8c68
Logan Dhillon <logandhi64@gmail.com>
2023-12-09 14:18:29 -0500

    Clean up code from `KD-pdf-parsing`

--- translate.py
+++ translate.py
@@ -2,12 +2,15 @@ import subprocess
 import os
 import markdownify
 
+
 def convert_pdf_to_html(pdf_path, output_dir):
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    command = ['pdftohtml', '-s', pdf_path, os.path.join(output_dir, 'output.html')]
+    command = ['pdftohtml', '-s', pdf_path,
+               os.path.join(output_dir, 'output.html')]
     subprocess.run(command)
+
 
 def markdownify(path):
     with open(path, 'r') as f:
@@ -15,9 +18,8 @@ def markdownify(path):
     markdown = markdownify.markdownify(html)
     return markdown
 
+
 pdf_path = 'test.pdf'  # Replace with your PDF file path
-output_dir = r'C:\Users\egeke\Desktop\Development Files\python-codestone\test'    # Replace with your desired output directory
-
-
+output_dir = "out"    # Replace with your desired output directory
 
 convert_pdf_to_html(pdf_path, output_dir)
